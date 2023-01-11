7701f4a6219a2aa20123e56a26b52e11324ba320
Paul Lugo <paulugortega@gmail.com>
2023-01-11 12:28:49 -0700

    Modificación a la lógica del encriptador

--- js/logica.js
+++ js/logica.js
@@ -1,15 +1,15 @@
-function esconder(){
-    document.getElementById("imagen").style.display = "none";
-    document.getElementById("mensaje").style.display = "none";
-    document.getElementById("mensaje-encriptado").style.display = "block";
-    document.getElementById("boton-copiar").style.display = "block";
+function esconder() {
+  document.getElementById("imagen").style.display = "none";
+  document.getElementById("mensaje").style.display = "none";
+  document.getElementById("mensaje-encriptado").style.display = "block";
+  document.getElementById("boton-copiar").style.display = "block";
 }
 
-function mostrar(){
-    document.getElementById("imagen").style.display = "block";
-    document.getElementById("mensaje").style.display = "block";
-    document.getElementById("mensaje-encriptado").style.display = "none";
-    document.getElementById("boton-copiar").style.display = "none";
+function mostrar() {
+  document.getElementById("imagen").style.display = "block";
+  document.getElementById("mensaje").style.display = "block";
+  document.getElementById("mensaje-encriptado").style.display = "none";
+  document.getElementById("boton-copiar").style.display = "none";
 }
 
 const btnEncriptar = document.getElementById("btn-encriptar"); //Tomando el botón desde HTML.
@@ -20,93 +20,61 @@ let textarea = document.getElementById("texto"); //Tomando datos de textarea.
 let mensajePuro = []; //servirá para guardar el texto del textarea.
 
 const parrafo = document.getElementById("mostrar-encriptado"); //Parrafo para mostrar resultado.
-let textofinal; //Creamos e inicializamos como vacía la cadena de texto que guarda el encriptado. 
+let textoFinal; //Creamos e inicializamos como vacía la cadena de texto que guarda el encriptado.
 
-mostrar()
+mostrar();
 
-const encriptar = e => {
-    textofinal = "";
-    let acentosA = textarea.value.toLowerCase();  //Para convertir mayúsculas en minúsculas.
-    let acentosE = acentosA.replace(/[áäâà]/g,'a'); //Para quitar acentos.
-    let acentosI = acentosE.replace(/[éëêè]/g,'e');
-    let acentosO = acentosI.replace(/[íïîì]/g,'i');
-    let acentosU = acentosO.replace(/[óöôò]/g,'o');
-    mensajePuro = acentosU.replace(/[úüûù]/g,'u'); //Guardamos el texto ingresado en la variable mensajePuro.
+const encriptar = (e) => {
+  textoFinal = "";
+  let acentos = textarea.value.toLowerCase(); //Para convertir mayúsculas en minúsculas.
+  mensajePuro = acentos
+    .replace(/[áäâà]/g, "a") //Para quitar acentos y guardar en variable mensajePuro.
+    .replace(/[éëêè]/g, "e")
+    .replace(/[íïîì]/g, "i")
+    .replace(/[óöôò]/g, "o")
+    .replace(/[úüûù]/g, "u"); 
 
-    console.log(mensajePuro);
-    for(let i = 0; i < mensajePuro.length; i++){
-        
-        if(mensajePuro[i] == "e" && mensajePuro[i+1] != "n" && mensajePuro[i+2] != "t"
-        && mensajePuro[i+3] != "e" && mensajePuro[i+4] != "r"){
-            textofinal = textofinal + "enter"; //Guardamos la letra codificada en la variable.
-        }else if(mensajePuro[i] == "i" && mensajePuro[i+1] != "m" && 
-        mensajePuro[i+2] != "e" && mensajePuro[i+3] != "s"){
-            textofinal = textofinal + "imes";
-        }else if(mensajePuro[i] == "a" && mensajePuro[i+1] != "i"){
-            textofinal = textofinal + "ai";
-        }else if(mensajePuro[i] == "o" && mensajePuro[i+1] != "b" && 
-        mensajePuro[i+2] != "e" && mensajePuro[i+3] != "r"){
-            textofinal = textofinal + "ober";
-        }else if(mensajePuro[i] == "u" && mensajePuro[i+1] != "f" && 
-        mensajePuro[i+2] != "a" && mensajePuro[i+3] != "t"){
-            textofinal = textofinal + "ufat";
-        }else{
-            textofinal = textofinal + mensajePuro[i];
-        }
-    }
+  //Reemplazamos las letras.
+  textoFinal = mensajePuro
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/a/g, "ai")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat");
 
-    esconder();
-    parrafo.innerHTML = textofinal; //Mostrar resultado.
-    textarea.value = ""; //Borrar texto de textarea.
+  esconder();
+  parrafo.innerHTML = textoFinal; //Mostrar resultado.
+  textarea.value = ""; //Borrar texto de textarea.
+};
 
-} 
+const desencriptar = (e) => {
+  textoFinal = "";
 
+  let acentos = textarea.value.toLowerCase(); //Para convertir mayúsculas en minúsculas.
+  mensajePuro = acentos
+    .replace(/[áäâà]/g, "a") //Para quitar acentos y guardar en variable mensajePuro.
+    .replace(/[éëêè]/g, "e")
+    .replace(/[íïîì]/g, "i")
+    .replace(/[óöôò]/g, "o")
+    .replace(/[úüûù]/g, "u");
 
-const desencriptar = e => {
+  textoFinal = mensajePuro
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
 
+  esconder();
+  parrafo.innerHTML = textoFinal; //Mostrar resultado.
+  textarea.value = ""; //Borrar texto de textarea.
+};
 
-    textofinal = "";
-    mensajePuro = textarea.value; //Guardamos el texto ingresado en la variable mensajePuro.
-    
-    for(let i = 0; i < mensajePuro.length; i++){
-        
-        if(mensajePuro[i] == "e" && mensajePuro[i+1] == "n" && 
-        mensajePuro[i+2] == "t" && mensajePuro[i+3] == "e" && mensajePuro[i+4] == "r"){
-            textofinal = textofinal + "e"; //Guardamos la letra codificada en la variable.
-            i=i+4;
-        }else if(mensajePuro[i] == "i" && mensajePuro[i+1] == "m" && 
-        mensajePuro[i+2] == "e" && mensajePuro[i+3] == "s"){
-            textofinal = textofinal + "i";
-            i=i+3;
-        }else if(mensajePuro[i] == "a" && mensajePuro[i+1] == "i"){
-            textofinal = textofinal + "a";
-            i=i+1;
-        }else if(mensajePuro[i] == "o" && mensajePuro[i+1] == "b" && 
-        mensajePuro[i+2] == "e" && mensajePuro[i+3] == "r"){
-            textofinal = textofinal + "o";
-            i=i+3;
-        }else if(mensajePuro[i] == "u" && mensajePuro[i+1] == "f" && 
-        mensajePuro[i+2] == "a" && mensajePuro[i+3] == "t"){
-            textofinal = textofinal + "u";
-            i=i+3;
-        }else{
-            textofinal = textofinal + mensajePuro[i];
-        }
-    }
-
-    esconder();
-    parrafo.innerHTML = textofinal; //Mostrar resultado.
-    textarea.value = ""; //Borrar texto de textarea.
-
-}
-
-const copiar = e => {
-    
-    textarea.value = textofinal;  //copia el texto encriptado y lo muestra en el textarea.
-    parrafo.innerHTML = "";
-    mostrar();
-    
-}
+const copiar = (e) => {
+  textarea.value = textoFinal; //copia el texto encriptado y lo muestra en el textarea.
+  parrafo.innerHTML = "";
+  mostrar();
+};
 
 btnEncriptar.onclick = encriptar; //Evento para detectar click en botón.
 btnDesencriptar.onclick = desencriptar;
